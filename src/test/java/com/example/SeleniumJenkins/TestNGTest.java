c5900690eca264803106892057e5b087f0bbcd11
Kiran <Kiran@Kiran-PC>
2015-12-28 23:18:30 +0100

    Changed it to execute in Saucelabs

--- src/test/java/com/example/SeleniumJenkins/TestNGTest.java
+++ src/test/java/com/example/SeleniumJenkins/TestNGTest.java
@@ -4,29 +4,47 @@ import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class TestNGTest {
-	
-	private WebDriver driver;
-	
-  @Test
-  public void f() {
-	  driver.get("http://www.guru99.com/selenium-tutorial.html");  
-      String title = driver.getTitle();                
-      AssertJUnit.assertTrue(title.contains("Free Selenium Tutorials"));
-  }
-  @BeforeTest
-  public void beforeTest() {
-	  driver = new FirefoxDriver();
-  }
 
-  @AfterTest
-  public void afterTest() {
-	  driver.quit();  
-  }
+	public static final String USERNAME = "skirankumars";
+	public static final String ACCESS_KEY = "aed6d35f-ec8d-4f82-a641-4a095e32abf0";
+	public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+	private RemoteWebDriver driver;
+
+	@Test
+	public void f() {
+		driver.get("http://www.guru99.com/selenium-tutorial.html");
+		String title = driver.getTitle();
+		AssertJUnit.assertTrue(title.contains("Free Selenium Tutorials"));
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		DesiredCapabilities caps = DesiredCapabilities.firefox();
+		caps.setCapability("platform", "Windows 7");
+		caps.setCapability("version", "42");
+		try {
+			this.driver = new RemoteWebDriver(new URL(URL),caps);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
 
 }
